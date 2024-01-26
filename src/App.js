51ef21ee07eb83d6f94db1a5c07459fe06b17f63
Udaya Kumar M <udayaa5209v@gmail.com>
2024-01-27 00:55:20 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Om Muruga</h1>
+        <h2>Kandhan Karunai</h2>
+        <h3>Vetri Vel Muruganuku Aarogara</h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
